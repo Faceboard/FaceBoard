df014b38e2a941a2425de464bd92c33d63a9c31a
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-08-11 13:31:25 -0400

    (feat) add invitation to session

--- client/sync.js
+++ client/sync.js
@@ -4,22 +4,26 @@ let options = {
   'force new connection': true
 };
 
-let socket = io('http://localhost:3000/test', options);
-// let socket = io('https://face-board.herokuapp.com/test', options);
+// let socket = io('http://localhost:3000/test', options);
+let socket = io('https://face-board-pr-23.herokuapp.com/test', options);
 
-socket.on('userWantsToCreateSession', function (roomname) {
+socket.on('userWantsToCreateSession', function (data) {
+  if (global.localStorage.username === data.secondUserName) {
+    confirm(data.firstUserName + ' wants to create a private session with you. Would you like to join?') ?
+      socket.emit('userWantsToJoinSession', data) : null;
+  }
   console.log('THIS IS THE USER WANTS TO CREATE SESSION EVENT', roomname);
   socket.emit('testing', roomname);
 });
+
+socket.on('userHasJoinedSession', function (mes) {
+  console.log(mes);
+})
 
 socket.on('user connected', function (data) {
   console.log(data);
   socket.emit('make sesssion', "User has connected");
 });
-
-socket.on('privateTest', function (roomname) {
-  console.log('THIS IS A PRIVATE TEST IN ', roomname);
-})
 
 
 export default socket;
