ec2ff9b6264bbeda0d797eaf458882d031b7091e
Ron Cruz <Ronolibert@gmail.com>
2016-08-13 13:55:39 -0700

    (fix) fix merge conflicts

--- client/components/videos.js
+++ client/components/videos.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { constraints, successCallback, errorCallback, initOffer } from '../actions/webrtc';
 
 class Videos extends React.Component {
   constructor(props) {
     super(props);
   }
 
+<<<<<<< d156d6c0df28d00262d5a191adf674a336fba4a9
+=======
+  componentDidMount() {
+    initOffer();
+  }
+
+>>>>>>> (feat) add local session description and prepare to send an offer
   render() {
     return (
       <video id="localVideo">
