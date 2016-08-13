d156d6c0df28d00262d5a191adf674a336fba4a9
Ron Cruz <Ronolibert@gmail.com>
2016-08-13 13:53:52 -0700

    (fix) re-add video component

--- client/components/videos.js
+++ client/components/videos.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import { constraints, successCallback, errorCallback } from '../actions/webrtc';
 
 class Videos extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
-  componentDidMount () {
-    navigator.webkitGetUserMedia(constraints, successCallback, errorCallback);
-  }
-
-  render () {
+  render() {
     return (
       <video id="localVideo">
       </video>
-    );
+    )
   }
 }
 
