dc6c30eb9bb3d7dae303ccec215bf3407dd899dc
Ron Cruz <Ronolibert@gmail.com>
2016-08-14 17:49:25 -0700

    (feat) add functioning video chatting

--- client/components/videos.js
+++ client/components/videos.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Pubnub } from '../actions/pubnub';
 
-class Videos extends React.Component {
-  constructor(props) {
-    super(props);
-    global.localStorage.test = false;
-  }
-
-  render() {
-    return (
-      <div id="remoteVideo">
-      </div>
-    )
-  }
-}
+const Videos = () => (
+  <div id="remoteVideo">
+  </div>
+)
 
 export default Videos;
--- client/sync.js
+++ client/sync.js
@@ -11,10 +11,11 @@ let options = {
 let socket = io('https://face-board-pr-31.herokuapp.com/test', options);
 
 socket.on('userHasJoinedSession', function (mes) {
+  // pubnub config
   global.phone = PHONE({
-    number        : global.localStorage.username,
-    publish_key   : 'pub-c-561a7378-fa06-4c50-a331-5c0056d0163c',
-    subscribe_key : 'sub-c-17b7db8a-3915-11e4-9868-02ee2ddab7fe',
+    number: global.localStorage.username,
+    publish_key: 'pub-c-561a7378-fa06-4c50-a331-5c0056d0163c',
+    subscribe_key: 'sub-c-17b7db8a-3915-11e4-9868-02ee2ddab7fe',
     media: {
       audio: true,
       video:
@@ -23,7 +24,7 @@ socket.on('userHasJoinedSession', function (mes) {
         width:280
       }
     },
-    ssl           : true
+    ssl: true
   });
 
   phone.ready(function() {
