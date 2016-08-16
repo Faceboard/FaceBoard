697343db1156035fb836fcdedeab138f5629b648
Jason Bland <jtbland@gmail.com>
2016-08-16 16:35:57 -0700

    (feat) remove canvas-desginer and add awwapp

--- client/components/mainSession.js
+++ client/components/mainSession.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { configFirebase, fetchFirepad, deleteFirepad } from '../actions/firebaseConfig';
-import Designer from '../actions/whiteboardConfig';
-
 
 class MainSession extends React.Component {
   constructor (props) {
@@ -11,8 +9,23 @@ class MainSession extends React.Component {
   }
 
   componentDidMount () {
+    var aww = new AwwBoard('#whiteboard', {
+      apiKey: '4e0bde7f-d2c2-438f-a2af-a3cd3ab506ae',
+      autoJoin: true,
+      boardLink: 'face-board-awwapp',
+      sizes: [3, 5, 8, 13, 20],
+      fontSizes: [10, 12, 16, 22, 30],
+      menuOrder: ['colors', 'sizes', 'tools', 'admin',
+        'utils'],
+      tools: ['pencil', 'eraser', 'text', 'image', 'undo', 'trash'],
+      colors: [ "#000000", "#f44336", "#4caf50", "#2196f3",
+        "#ffc107", "#9c27b0",     "#e91e63", "#795548"],
+      defaultColor: "#000000",
+      defaultSize: 8,
+      defaultTool: 'pencil'
+    });
+
     this.props.dispatch(fetchFirepad());
-    Designer.appendTo(document.getElementById('whiteboard'));
   }
 
   componentWillUnmount () {
