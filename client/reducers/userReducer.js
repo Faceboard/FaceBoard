78a943cf499ddd1ef830cc90d0fec55f64b3237b
Patrick Truong <pahtruon@gmail.com>
2016-08-15 17:35:44 -0700

    (feat) add friends reducer

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,7 +1,8 @@
-import { FETCHING_USERS, USERS_FETCHED, FETCH_USERS_ERROR, CHANGE_FIELD } from '../actions/action';
+import { FETCHING_USERS, USERS_FETCHED, FETCH_USERS_ERROR, CHANGE_FIELD, FETCHING_FRIENDS, FRIENDS_FETCHED, FETCHING_FRIENDS_ERROR } from '../actions/action';
 
 const initialState = {
   users: [],
+  friends: [],
   fetching: false,
   fetched: false
 };
@@ -18,6 +19,20 @@ const userReducer = (state = initialState, action) => {
         users: action.payload
       });
     case FETCH_USERS_ERROR:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: true,
+        error: action.payload
+      });
+    case FETCHING_FRIENDS:
+      return Object.assign({}, state, {fetching: true});
+    case FRIENDS_FETCHED:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: true,
+        friends: action.payload
+      });
+    case FETCHING_FRIENDS_ERROR:
       return Object.assign({}, state, {
         fetching: false,
         fetched: true,
