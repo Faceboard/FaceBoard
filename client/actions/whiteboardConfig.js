73b343d8c939dfb58de588f4fe46d97cd5cb378b
Jason Bland <jtbland@gmail.com>
2016-08-15 22:42:12 -0700

    (feat) add rtcmulticonnect basic options

--- client/actions/whiteboardConfig.js
+++ client/actions/whiteboardConfig.js
@@ -4,5 +4,16 @@ const Connection = window.connection;
 Designer.widgetHtmlURL = 'https://cdn.webrtc-experiment.com/Canvas-Designer/widget.html';
 Designer.widgetJsURL = 'https://cdn.webrtc-experiment.com/Canvas-Designer/widget.js';
 
+Connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+Connection.socketMessageEvent = 'canvas-designer';
+Connection.enableFileSharing = false;
+Connection.session = {
+  data: true
+};
+Connection.sdpConstraints.mandatory = {
+  OfferToReceiveAudio: false,
+  OfferToReceiveVideo: false
+};
+
 export default Designer;
 export default Connection;
