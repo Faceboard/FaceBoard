5fd08a6d7f8f6ff9ac3ca7a07f9c5d80d476b45f
Jason Bland <jtbland@gmail.com>
2016-08-15 23:02:46 -0700

    (feat) add connection methods to send/receive data

--- client/actions/whiteboardConfig.js
+++ client/actions/whiteboardConfig.js
@@ -15,5 +15,24 @@ Connection.sdpConstraints.mandatory = {
   OfferToReceiveVideo: false
 };
 
+Connection.onopen = function(event) {
+  if(Designer.pointsLength <= 0) {
+    setTimeout(function() {
+      Connection.send('plz-sync-points');
+    }, 1000);
+  }
+};
+
+Connection.onmessage = function(event) {
+  if(event.data === 'plz-sync-points') {
+    Designer.sync();
+    return;
+  }
+
+  if(event.data.Designer) {
+    Designer.syncData( event.data.actualData );
+  }
+};
+
 export default Designer;
 export default Connection;
