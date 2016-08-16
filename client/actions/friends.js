5b76ed86c27cf46eda7e2bbb980b31102a8312cb
Patrick Truong <pahtruon@gmail.com>
2016-08-15 17:35:29 -0700

    (feat) add actions for friendlist

--- client/actions/friends.js
+++ client/actions/friends.js
@@ -1,13 +1,44 @@
 import axios from 'axios';
+import { FETCHING_FRIENDS, FRIENDS_FETCHED, FETCHING_FRIENDS_ERROR } from './action';
+
+// export function getAllFriends () {
+//   // still requires and authentication token
+//   return axios.get('https://face-board.herokuapp.com/friends/findAll');
+// };
+
+// export function addFriend (friendid, friendname) {
+//   return axios.post('https://face-board.herokuapp.com/friends/add', {
+//     friendid,
+//     friendname
+//   });
+// };
+
 
 export function getAllFriends () {
-  // still requires and authentication token
-  return axios.get('https://face-board.herokuapp.com/friends/findAll');
+  return function (dispatch) {
+    dispatch({type: FETCHING_FRIENDS });
+    axios.get('https://face-board.herokuapp.com/friends/findAll')
+      .then((response) => {
+        dispatch({type: FRIENDS_FETCHED, payload: response.data });
+      })
+      .catch((error) => {
+        dispatch({type: FETCHING_FRIENDS_ERROR, error: error });
+      });
+  };
 }
 
 export function addFriend (friendid, friendname) {
-  return axios.post('https://face-board.herokuapp.com/friends/add', {
-    friendid,
-    friendname
-  });
+  return function (dispatch) {
+    dispatch({type: FETCHING_FRIENDS});
+    axios.post('https://face-board.herokuapp.com/friends/add', {
+      friendid,
+      friendname
+    })
+    .catch((error) => {
+      dispatch({
+        type: FETCHING_FRIENDS_ERROR,
+        payload: error
+      });
+    });
+  }
 }
