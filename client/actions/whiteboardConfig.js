e44c2317a30f37781000db3e57800882d56d672e
Jason Bland <jtbland@gmail.com>
2016-08-15 22:38:54 -0700

    (feat) add rtcmulticonnection object

--- client/actions/whiteboardConfig.js
+++ client/actions/whiteboardConfig.js
@@ -1,6 +1,8 @@
 const Designer = window.designer;
+const Connection = window.connection;
 
 Designer.widgetHtmlURL = 'https://cdn.webrtc-experiment.com/Canvas-Designer/widget.html';
 Designer.widgetJsURL = 'https://cdn.webrtc-experiment.com/Canvas-Designer/widget.js';
 
 export default Designer;
+export default Connection;
