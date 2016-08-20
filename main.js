c96e64eef2430a1c35599734957b86979f7c9628
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-08-20 14:46:02 -0400

    (fix) update variable name in before-quit

--- main.js
+++ main.js
@@ -78,13 +78,14 @@ app.on('ready', function () {
 
   win.loadURL(`file://${__dirname}/client/static/index.html`);
 
+  app.on('before-quit', function () {
+    delete win.inSession;
+  });
+
   app.on('closed', function () {
     win = null;
   });
 
-  app.on('before-quit', function () {
-    delete window.inSession;
-  })
 });
 
 
