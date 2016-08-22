9c943b2425b13895e17c5b7ca8697acba44435bd
Patrick Truong <pahtruon@gmail.com>
2016-08-21 21:01:47 -0700

    (feat) remove delete friend from menu to button (very basic style)

--- client/components/friendsList.js
+++ client/components/friendsList.js
@@ -6,7 +6,7 @@ import { withRouter } from 'react-router';
 import { inviteToSession, makeSession, sessionChange, makePrivateSession, askSecondUserToJoin } from '../actions/session';
 import socket from '../sync';
 import { makeMenu, reattachMenus } from '../actions/menu';
-import { addFriend, getAllFriends } from '../actions/friends';
+import { addFriend, getAllFriends, deleteFriend } from '../actions/friends';
 import { getPrivateMessages, getAllFriendPrivateMsg, pChatStart } from '../actions/chat';
 
 class FriendsList extends React.Component {
@@ -31,15 +31,31 @@ class FriendsList extends React.Component {
     reattachMenus();
   }
 
+  addPerson (e) {
+    e.preventDefault();
+    if (e.target.innerHTML !== global.localStorage.username) {
+      this.props.dispatch(addFriend(e.target.value, e.target.innerHTML));
+    }
+  }
+
   privateMessageStart (e) {
     const { router } = this.props;
     pChatStart(e, router);
   }
 
+  removeFriend (e) {
+    deleteFriend(e.target.value);
+  }
+
   render () {
     const { friends } = this.props;
-    const mapFriends = friends.map(friend => <li onClick={this.privateMessageStart.bind(this)}
-      className="friends list-group-item fa fa-star-o" key={friend.id} value={friend.friendid}>{friend.friendname}</li>);
+    const mapFriends = friends.map(friend =>
+      <div className='list-group-item'>
+        <li onClick={this.privateMessageStart.bind(this)}
+        className="friends" key={friend.id} value={friend.friendid}>{friend.friendname}</li>
+        <button className="icon icon-cancel-circled" onClick={this.removeFriend.bind(this)} value={friend.friendname}></button>
+      </div>
+    );
 
     if (!friends.length) {
       return (
